9c46f2bb529de2c86ff37e507d921e3b7731fb16
Dasha Mihai <dashamihai@mail.ru>
2018-08-14 21:01:38 +0300

    add task 15

---
+++ task15/Home_15.java
@@ -0,0 +1,16 @@
+package home;
+
+public class Home_15 {
+
+	public static void main(String[] args) throws Exception {
+		Massive massive = new Massive();
+		System.out.println(massive.getMas());
+		System.out.println(massive.MasMult());
+		System.out.println(massive.MasZero());
+		System.out.println(massive.MaxIndex());
+		System.out.println(massive.MinIndex());
+		System.out.println(massive.MasPlus());
+
+	}
+
+}
