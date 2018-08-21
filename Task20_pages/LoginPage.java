2c8b52e69aef9f6b8167842c190c5cca02aaac0d
Dasha Mihai <dashamihai@mail.ru>
2018-08-21 23:10:29 +0300

    add tas20 pages

---
+++ Task20_pages/LoginPage.java
@@ -0,0 +1,45 @@
+package page;
+
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.FindBy;
+	import org.openqa.selenium.support.PageFactory;
+
+	public class LoginPage
+	{
+	    @FindBy(id = "mailbox:login")
+	    private WebElement loginField;
+
+	    @FindBy(id = "mailbox:password")
+	    private WebElement passwordField;
+
+	    @FindBy(xpath = "//*[@id='mailbox:submit']/input")
+	    private WebElement buttonEnter;
+
+	    @FindBy(xpath = "//*[@id='PH_logoutLink']")
+	    private WebElement logoutLink;
+
+	    public LoginPage(WebDriver webdriver)
+	    {
+	        PageFactory.initElements(webdriver, this);
+	    }
+
+	    public void enterLoginAndPass(String login, String password)
+	    {
+	        loginField.clear();
+	        loginField.sendKeys(login);
+	        passwordField.clear();
+	        passwordField.sendKeys(password);
+	    }
+
+	    public void clickEnterButton()
+	    {
+	        buttonEnter.click();
+	    }
+
+	    public boolean logoutLinkPresents()
+	    {
+	        return logoutLink.isDisplayed();
+	    }
+	}
---
+++ Task20_pages/MessageInSpam.java
@@ -0,0 +1,39 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MessageInSpam {
+	 private WebDriver driver;
+	 
+	@FindBy(xpath = "(//*[@class='b-checkbox__box'])[10]")
+    private WebElement checkboxMessage1;    
+    
+    @FindBy(xpath = "(//*[text()='Спам'])[1]")
+    private WebElement spamButton;
+    
+    @FindBy(xpath = "//a[text()='Отменить']")
+    private WebElement messageSpamLink;
+    
+    public MessageInSpam(WebDriver webdriver)
+    {
+        PageFactory.initElements(webdriver, this);
+        this.driver = webdriver;
+    }
+   
+	public void clickCheckboxMessage1() {
+		(new WebDriverWait(driver, 15)).until(ExpectedConditions.visibilityOf(checkboxMessage1)).click();
+	}
+
+	public void clickSpamButton() {
+		spamButton.click();
+	}
+	 public boolean MessageInSpamPresents() {
+			return messageSpamLink.isDisplayed();
+		}
+
+}
